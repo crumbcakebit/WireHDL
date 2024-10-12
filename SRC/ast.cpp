314f5a7f7df064abc8c8ad018523cfdb4f0866a1
crumbcakebit <114716069+crumbcakebit@users.noreply.github.com>
2024-10-11 23:30:32 -0400

    Delete SRC directory

--- SRC/ast.cpp
+++
@@ -1,7 +0,0 @@
-#include "ast.h"
-
-ModuleNode::ModuleNode(const std::string& name) : name(name) {}
-
-void ModuleNode::addInput(std::unique_ptr<ASTNode> input) {
-    inputs.push_back(std::move(input));
-}
